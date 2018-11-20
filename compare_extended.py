ab5d68c1376def7cf6b316217b69096d0e8342d4
Ureña Carrion Javier <urenaj1@pe2.int.triton.aalto.fi>
2018-11-20 21:26:01 +0200

    minor bugs

--- compare_extended.py
+++ compare_extended.py
@@ -14,9 +14,9 @@ del degs['ovrl']
 ext_degs = pd.read_table(path + 'extended_neighbors.txt', sep=' ')
 del ext_degs['ovrl']
 ext_degs.rename({'deg_0': 'edeg_0', 'deg_1':'edeg_1', 'n_ij':'en_ij'}, axis=1, inplace=True)
-degs = pd.merge(ext_degs, how='left', on=['0', '1'])
-degs['k_nn'] = float(degs['deg_0']) + degs['deg_1'] - degs['n_ij']
-degs['ek_nn'] = float(degs['edeg_0']) + degs['edeg_1'] - degs['en_ij']
+degs = pd.merge(degs, ext_degs, how='left', on=['0', '1'])
+degs['k_nn'] = degs['deg_0'] + degs['deg_1'] - 2 - degs['n_ij']
+degs['ek_nn'] = degs['edeg_0'] + degs['edeg_1'] - 2 - degs['en_ij']
 degs['k_nn_f'] = degs['k_nn']/degs['ek_nn']
-degs['k_n_f'] = float(degs['n_ij'])/degs['en_ij']
-degs.to_csv(path + 'neigbors_comparison.txt', index=False)
+degs['k_n_f'] = degs['n_ij']/degs['en_ij']
+degs.to_csv(path + 'neighbors_comparison.txt', index=False, sep=' ')
