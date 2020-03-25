4222c9cac703c6f3c6c33489d40b74d93ddde6a2
javurena7 <javurena7@gmail.com>
2020-03-25 18:49:11 +0200

    Add main

--- paper_plots.py
+++ paper_plots.py
@@ -105,7 +105,9 @@ def plot_ts(times, obs_w, ax=None, obs_w_l=.15, levs=.1):
     ax.axis('off')
 
 
-def plot_main_figure():
+edge_values = {'b':{0:0, 2:0}, 'bt_n':{0:0, 2:0}, 't_stb':{0:0, 2:0}}
+
+def plot_main_figure(edge_set, edge_values):
     fig = plt.figure(tight_layout=True)
     widths = [1, 1, 1]
     heights = [2.5, 1, 2.5, 1]
@@ -114,28 +116,48 @@ def plot_main_figure():
     ##### TODO: remove this
     times = [1167751219, 1167773133, 1168025203, 1168191776, 1168350076, 1168721201, 1169472190, 1169732333]
     obs_w = [1167751219 - 3600 * 24 * 3, 1169732333 + 3600 * 24 * 10]
+    col_var = ['b', 'bt_n', 't_stb']
     for col in range(3):
+        var = col_var[col]
         for row in [0, 2]:
             ax = fig.add_subplot(spec[row, col])
-            plot_net(ax=ax)
+            plot_net(edge_set[var][row][0], ax=ax)
 
             ax2 = fig.add_subplot(spec[row + 1, col])
             plot_ts(times, obs_w, ax=ax2)
 
 def get_edge_set(df):
-    vrs = ['mu', 'b', 't_stb', 'bt_n', 'out_call_div']
+    """
+    Given the dataframe for the whole data, gets possible edges for the main plot according to weak/strong values
+    """
+    vrs = ['b', 't_stb', 'bt_n', 'out_call_div']
+    data_vrs = ['0', '1', 'w', 'ovrl', 'n_ij', 'deg_0', 'deg_1']
     edges = {v: {} for v in vrs}
-    df = df[['0', '1', 'w', 'ovrl'] + vrs][(df.w > 59) & (df.w < 80)].sort_values('ovrl')
+    df = df[data_vrs + vrs][(df.w > 59) & (df.w < 80)].sort_values('ovrl')
 
     for var in vrs:
         asc = True if var in ['t_stb', 'bt_n'] else False
-        edges[var]['low'] = df[df.ovrl < .03].sort_values(var, ascending=asc)[['0','1']].head(10).values
-        edges[var]['high'] = df[df.ovrl > .16].sort_values(var, ascending=(not asc))[['0','1']].head(10).values
+        edges[var][0] = parse_edges(df[df.ovrl < .03].sort_values(var, ascending=asc).head(20))
+        edges[var][2] = parse_edges(df[df.ovrl > .16].sort_values(var, ascending=(not asc)).head(10))
 
     return edges
 
 
-def get_times(edge_set, times_path='../full_run/times_dict.txt'):
+def parse_edges(edge):
+    edges = {}
+    for rowi in edge.iterrows():
+        row = rowi[1]
+        n01 = row['n_ij']
+        n0 = row['deg_0'] - n01
+        n1 = row['deg_1'] - n01
+        edges[(int(row[0]), int(row[1]))] = {'n01': n01, 'n0': n0, 'n10': n1}
+    return edges
+
+
+def get_times(edges, times_path='../full_run/times_dict.txt'):
+    """
+    Given the edge set from get_edge_set, gets the timestamps for those edges
+    """
     import utils
     times_dict = {}
     edge_set = set([tuple(x) for sublist in edges.values() for y in sublist.values() for x in y])
@@ -143,7 +165,7 @@ def get_times(edge_set, times_path='../full_run/times_dict.txt'):
         row = r.readline()
         while (row) & (len(edge_set) > 0):
             e0, e1, times = utils.parse_time_line(row)
-            edge = [e0, e1]
+            edge = (e0, e1)
             if edge in edge_set:
                 times_dict[edge] = times
                 edge_set.remove(edge)
@@ -151,7 +173,25 @@ def get_times(edge_set, times_path='../full_run/times_dict.txt'):
     return times_dict
 
 
+if __name__ == '__main__':
+    import pandas as pd
+    import json
+    path = '/scratch/work/urenaj1/full/'
 
+    df_path = path + 'full_df_paper.txt'
+    times_path = path + 'times_dict.txt'
+
+    edges_outpath = path + 'mainplot_edges.json'
+    times_outpath = path + 'mainplot_times.json'
+
+    df = pd.read_csv(df_path, sep=' ')
+    del df
+
+    edges = get_edge_set(df)
+    times = get_times(edges, times_path)
+
+    json.dump(edges, open(edges_outpath, 'w'))
+    json.dump(times, open(times_outpath, 'w'))
 
 
 
