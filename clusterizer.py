99223fc961153a3aab3f41a67b6834ad0973317e
javurena7 <javurena7@gmail.com>
2020-03-25 14:17:28 +0200

    Add clusterizer thing

--- clusterizer.py
+++ clusterizer.py
@@ -6,13 +6,19 @@ Script for clusterizing given the ids
 """
 
 clust_ids_path = '../full_run/reclusters_ids.p', 'r'
+ids_path = '../full_run/full_df_paper_ids.txt'
 week_df_path = '../full_run/week_vec_call.txt'
 output_path = '../full_run/clustered_df.txt'
 
 
 
-def clusters_to_df(df_path, clusters_path, output_path):
+def clusters_to_df(df_path, clusters_path, output_path, ids=None):
     df = pd.read_csv(df_path, sep=' ')
+
+    if ids is not None:
+        ids = df.read_csv(ids, sep=' ')
+        df = ids.merge(df, on=['0', '1'], how='inner')
+
     clusters = json.load(open(clusters_path, 'r'))
 
     df_new = df[['0', '1']].copy()
