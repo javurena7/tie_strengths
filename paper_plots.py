922e5d00605a6af7bce6c2b83bc86dd5ae26d8f3
javurena7 <javurena7@gmail.com>
2020-03-25 17:09:25 +0200

    Add main plot

--- paper_plots.py
+++ paper_plots.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt; plt.ion()
+import matplotlib.gridspec as gridspec
 import numpy as np
 import networkx as nx
 
@@ -38,13 +39,13 @@ def edge_colormaps(net):
 
 
 def net_colormap(specs):
-    n_size = [200] * 2
+    n_size = [20] * 2
     n_col = ['orangered'] * 2
 
-    n_size += [150] * specs['n01']
+    n_size += [15] * specs['n01']
     n_col += ['royalblue'] * specs['n01']
 
-    n_size += [100] * (specs['n0'] + specs['n1'])
+    n_size += [10] * (specs['n0'] + specs['n1'])
     n_col += ['slategrey'] * (specs['n0'] + specs['n1'])
     return n_size, n_col
 
@@ -57,7 +58,7 @@ def plot_net(specs = {'n01': 5, 'n0': 36, 'n1': 50}, adj=.2, ax=None):
     edge_color = edge_colormaps(net)
     pos = nx.spring_layout(net)
     pos = adjust_pos(pos, specs, adj)
-    nx.draw_networkx(net, pos=pos, ax=ax, node_size=node_size, node_color=node_color, edge_color=edge_color, width=2, with_labels=False)
+    nx.draw_networkx(net, pos=pos, ax=ax, node_size=node_size, node_color=node_color, edge_color=edge_color, width=1, with_labels=False)
     ax.axis('off')
 
 
@@ -79,7 +80,7 @@ def plot_ts(times, obs_w, ax=None, obs_w_l=.15, levs=.1):
     ltimes = len(times)
     times = [obs_w[0]] + times + [obs_w[1]]
     times = [(t - obs_w[0])/(obs_w[1] - obs_w[0] + 0.) for t in times]
-    if fax is None:
+    if ax is None:
         fig, ax = plt.subplots(figsize=(8.8, 4))
     obs_w_l = obs_w_l
     levs = levs
@@ -104,8 +105,52 @@ def plot_ts(times, obs_w, ax=None, obs_w_l=.15, levs=.1):
     ax.axis('off')
 
 
-def plot_main_figure(obs_w):
-    fig, axs = plt.subplots(2, 3, figsize=(9, 6))
+def plot_main_figure():
+    fig = plt.figure(tight_layout=True)
+    widths = [1, 1, 1]
+    heights = [2.5, 1, 2.5, 1]
+
+    spec = gridspec.GridSpec(ncols=3, nrows=4, width_ratios=widths, height_ratios=heights, hspace=.25, wspace=.1)
+    ##### TODO: remove this
+    times = [1167751219, 1167773133, 1168025203, 1168191776, 1168350076, 1168721201, 1169472190, 1169732333]
+    obs_w = [1167751219 - 3600 * 24 * 3, 1169732333 + 3600 * 24 * 10]
+    for col in range(3):
+        for row in [0, 2]:
+            ax = fig.add_subplot(spec[row, col])
+            plot_net(ax=ax)
+
+            ax2 = fig.add_subplot(spec[row + 1, col])
+            plot_ts(times, obs_w, ax=ax2)
+
+def get_edge_set(df):
+    vrs = ['mu', 'b', 't_stb', 'bt_n', 'out_call_div']
+    edges = {v: {} for v in vrs}
+    df = df[['0', '1', 'w', 'ovrl'] + vrs][(df.w > 59) & (df.w < 80)].sort_values('ovrl')
+
+    for var in vrs:
+        asc = True if var in ['t_stb', 'bt_n'] else False
+        edges[var]['low'] = df[df.ovrl < .03].sort_values(var, ascending=asc)[['0','1']].head(10).values
+        edges[var]['high'] = df[df.ovrl > .16].sort_values(var, ascending=(not asc))[['0','1']].head(10).values
+
+    return edges
+
+
+def get_times(edge_set, times_path='../full_run/times_dict.txt'):
+    import utils
+    times_dict = {}
+    edge_set = set([tuple(x) for sublist in edges.values() for y in sublist.values() for x in y])
+    with open(times_path, 'r') as r:
+        row = r.readline()
+        while (row) & (len(edge_set) > 0):
+            e0, e1, times = utils.parse_time_line(row)
+            edge = [e0, e1]
+            if edge in edge_set:
+                times_dict[edge] = times
+                edge_set.remove(edge)
+            row = r.readline()
+    return times_dict
+
+
 
 
 
